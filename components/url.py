597e457d76d703d15c1b4eb1d9656d2f592767c6
Artur Adamian <arturadamian@gmail.com>
2024-08-19 12:12:02 -0400

    Add base urls to the class

--- components/url.py
+++ components/url.py
@@ -1,3 +1,5 @@
 class URL:
     U_HAUL = 'https://www.uhaul.com/'
     TARGET = 'https://www.target.com/'
+    GITHUB_UI = 'https://gh-users-search.netlify.app/'
+    GITHUB = 'https://api.github.com/'
